b201e8a95f60a424beee608cba4b3c5ef12a74b1
alagar0143 <alagarsamysamy74@gmail.com>
2025-12-05 14:09:16 +0530

    button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function demo(){
     if(x==1){
         console.log(document.querySelector('.book-apoi'));
         
-        document.querySelector('.book-apoi').style='width: fit-content;height: fit-content;z-index: 9;'
+        document.querySelector('.book-apoi').style='width: fit-content;height: fit-content;z-index: 9;padding:20px'
         x=2
     }
     else{
@@ -16,6 +16,6 @@ let btn=document.querySelector('.bo-ap');
 function sumit(){
     btn.innerText='Appoinment Booked'
     btn.style='background:green;color:white'
-    document.querySelector('.book-apoi').style='width:0;height: 0;z-index: 9;'
+    document.querySelector('.book-apoi').style='width:0;height: 0;z-index: 9;padding:0px'
     x=2
 }
