f3e5640852699cc7aab9e79bcdaf47e6cc229f36
alagar0143 <alagarsamysamy74@gmail.com>
2025-12-05 09:17:01 +0530

    change

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log('x');
+let x=1
+function demo(){
+    if(x==1){
+        console.log(document.querySelector('.book-apoi'));
+        
+        document.querySelector('.book-apoi').style='width: fit-content;height: fit-content;z-index: 9;'
+        x=2
+    }
+    else{
+        document.querySelector('.book-apoi').style='width:0;height: 0;z-index: 9;'
+        x=1
+    }
+}
+let btn=document.querySelector('.bo-ap');
+function sumit(){
+    btn.innerText='Appoinment Booked'
+    btn.style='background:green;color:white'
+    document.querySelector('.book-apoi').style='width:0;height: 0;z-index: 9;'
+    x=2
+}
